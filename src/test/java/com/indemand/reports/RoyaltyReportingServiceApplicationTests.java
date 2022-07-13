91333a7053b6a8bbfeae252a3bde4678adb54d2a
PhaniTulasi <ptulasi@indemand.com>
2022-07-13 15:17:06 -0400

    Initial

---
+++ src/test/java/com/indemand/reports/RoyaltyReportingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indemand.reports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoyaltyReportingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
